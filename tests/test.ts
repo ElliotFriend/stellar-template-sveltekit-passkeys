c314f57d5f59117cfbf92e1896490d19893cd9b6
Elliot Voris <elliot@stellar.org>
2024-11-12 11:35:24 -0600

    reconfigure some testing stuff, ignore upload step in action

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
     await page.goto('/');
-    await expect(page.getByRole('heading', { name: "Let's build a dapp!!" })).toBeVisible();
+    await expect(page.getByRole('heading', { name: "Hello, SvelteKit Passkeys!" })).toBeVisible();
 });
