04aff921fb1fb05ba62593a7dab8849abaceee4d
Elliot Voris <elliot@stellar.org>
2025-04-29 11:34:33 -0500

    update sdk version in bindings package

--- packages/hello_world/dist/index.js
+++ packages/hello_world/dist/index.js
@@ -1,5 +1,5 @@
 import { Buffer } from 'buffer';
-import { Client as ContractClient, Spec as ContractSpec } from '@stellar/stellar-sdk/contract';
+import { Client as ContractClient, Spec as ContractSpec, } from '@stellar/stellar-sdk/contract';
 if (typeof window !== 'undefined') {
     //@ts-ignore Buffer exists
     window.Buffer = window.Buffer || Buffer;
@@ -14,21 +14,17 @@ export const Errors = {};
 export class Client extends ContractClient {
     options;
     static async deploy(
-        /** Options for initalizing a Client as well as for calling a method, with extras specific to deploying. */
-        options,
-    ) {
+    /** Options for initalizing a Client as well as for calling a method, with extras specific to deploying. */
+    options) {
         return ContractClient.deploy(null, options);
     }
     constructor(options) {
-        super(
-            new ContractSpec([
-                'AAAAAAAAAAAAAAAFaGVsbG8AAAAAAAABAAAAAAAAAAJ0bwAAAAAAEAAAAAEAAAPqAAAAEA==',
-            ]),
-            options,
-        );
+        super(new ContractSpec([
+            'AAAAAAAAAAAAAAAFaGVsbG8AAAAAAAABAAAAAAAAAAJ0bwAAAAAAEAAAAAEAAAPqAAAAEA==',
+        ]), options);
         this.options = options;
     }
     fromJSON = {
-        hello: this.txFromJSON,
+        hello: (this.txFromJSON),
     };
 }
